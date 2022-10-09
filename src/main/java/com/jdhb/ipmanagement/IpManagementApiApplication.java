d0e450b2bfe676607f4353469846424dd012b349
Juan D Hernandez <juandanielhb@gmail.com>
2022-10-09 12:10:12 +0200

    Initial commit

---
+++ src/main/java/com/jdhb/ipmanagement/IpManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdhb.ipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdhb/ipmanagement/IpManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdhb.ipmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
